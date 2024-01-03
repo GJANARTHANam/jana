6dc14d2332d0e68c357d1ad1ae1419740a895531
GJANARTHANam <125451045+GJANARTHANam@users.noreply.github.com>
2024-01-03 12:32:47 +0530

    online bill payment

---
+++ src/configaration/Config.js
@@ -0,0 +1,7 @@
+
+// Back end connection and configaration
+
+export const config = {
+    api: "https://paytm-clone-backend.onrender.com", //"http://localhost:3000"
+    storage_key: "appoint_key"
+}
